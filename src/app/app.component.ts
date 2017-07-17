2585c71f23ade2f1b3479c7bf4a267ff37fe9434
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-07-17 08:44:05 -0700

    completed retro barcode generator assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Retro Barcode Generator';
+  colors: Array<string> = [
+    'BlueViolet',
+    'BurlyWood',
+    'CornflowerBlue',
+    'Cornsilk',
+    'DarkOliveGreen',
+    'DarkOrchid',
+    'DarkViolet',
+    'FloralWhite',
+    'ForestGreen',
+    'Fuchsia',
+    'HoneyDew',
+    'Indigo',
+    'Lavender',
+    'LavenderBlush',
+    'LawnGreen',
+    'LemonChiffon',
+    'Lime',
+    'LimeGreen',
+    'Linen',
+    'MediumOrchid',
+    'MediumVioletRed',
+    'MintCream',
+    'MistyRose',
+    'Olive',
+    'OliveDrab',
+    'Orange',
+    'OrangeRed',
+    'Orchid',
+    'PaleVioletRed',
+    'PapayaWhip',
+    'PeachPuff',
+    'Plum',
+    'RosyBrown',
+    'Thistle',
+    'Tomato',
+    'Violet',
+    'Wheat'
+  ];
+  randomColors = this.randomNumbers();
+  randomNumbers(){
+    let randomNumbers = [];
+    for (let i=0; i<10; ++i){
+      randomNumbers.push(Math.floor(Math.random()*38));
+    }
+    return randomNumbers;
+  }
 }
